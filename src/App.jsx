9ef11a1eb6c6a9ba29f75751cf5b7f1637813903
Neseli27 <neseli27@gmail.com>
2026-03-15 14:12:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1008,7 +1008,7 @@ const StaffPanel = ({ staff, onLogout }) => {
               ))}
             </>}
           </>
-        )}        )}
+        )}
       </div>
 
       {/* Garson sipariş popup */}
